5f74bc469d68ccb88f1ffb20dcc2445ca56db7a7
Christopher R. Johnson <crjohns@mit.edu>
2011-12-04 18:51:03 -0500

    Removed debug

--- user/sh.c
+++ user/sh.c
@@ -1,7 +1,7 @@
 #include <inc/lib.h>
 
 #define BUFSIZ 1024		/* Find the buffer overrun bug! */
-int debug = 1;
+int debug = 0;
 
 
 // gettoken(s, 0) prepares gettoken for subsequent calls and returns 0.
